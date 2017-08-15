bdde800c02241f6aabeae378f315fef8aea24e19
Tianyang Zhong <tianyangzhong2013@gmail.com>
2017-08-14 23:32:45 -0400

    build the ui and inject the views.

--- app/src/main/java/tianyangzhong/com/overdosed/MainActivity.java
+++ app/src/main/java/tianyangzhong/com/overdosed/MainActivity.java
@@ -5,16 +5,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @InjectView(R.id.alarmFreq) EditText mAlarmFreq;
+    @InjectView(R.id.ServiceSetBtn) Button mServiceSetBtn;
+    @InjectView(R.id.description) TextView mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -49,4 +63,12 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @OnClick(R.id.ServiceSetBtn)
+    public void startService(View view){
+        String numberInput = mAlarmFreq.getText().toString();
+        Toast.makeText(this, "the input is:"+ numberInput, Toast.LENGTH_LONG).show();
+
+    }
+
 }
